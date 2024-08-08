2e87246ace2e3aff6e5b20f5e6b97814fb474abc
MrKleivan <163395188+MrKleivan@users.noreply.github.com>
2024-08-08 11:19:11 +0200

    get

---
+++ myScript.js
@@ -0,0 +1,37 @@
+function s1funksjon(tagElement, alt1, alt2, alt3, alt4) {
+    if (document.getElementById(alt1).innerHTML == '') {
+        document.getElementById(alt1).innerHTML = tagElement.innerHTML;
+        tagElement.innerHTML = '';
+    }
+    if (document.getElementById(alt2).innerHTML == '') {
+        document.getElementById(alt2).innerHTML = tagElement.innerHTML;
+        tagElement.innerHTML = '';
+    }
+    if (document.getElementById(alt3).innerHTML == '') {
+        document.getElementById(alt3).innerHTML = tagElement.innerHTML;
+        tagElement.innerHTML = '';
+    }
+    if (document.getElementById(alt4).innerHTML == '') {
+        document.getElementById(alt4).innerHTML = tagElement.innerHTML;
+        tagElement.innerHTML = '';
+    }
+}
+
+let spillh1 = document.getElementById('spillh1');
+var spill1 = document.getElementById('spill1');
+spill1.style.display = 'none';
+
+var oppgave = document.getElementById('oppgave');
+oppgave.style.display = 'none';
+
+function spillvalg() {
+    spill1.style.display = 'block';
+    spillh1.innerHTML = 'Spill 1';
+    oppgave.style.display = 'none';
+}
+
+function oppgaver() {
+    spillh1.innerHTML = 'Oppgave';
+    spill1.style.display = 'none';
+    oppgave.style.display = 'block';
+}
