736033305f032ff9b616e4d51587676b5310cf94
MrKleivan <163395188+MrKleivan@users.noreply.github.com>
2024-08-08 12:02:27 +0200

    endring

--- myScript.js
+++ myScript.js
@@ -21,6 +21,8 @@ let spillh1 = document.getElementById('spillh1');
 var spill1 = document.getElementById('spill1');
 spill1.style.display = 'none';
 
+const tallid = document.getElementById('tallid');
+
 var oppgave = document.getElementById('oppgave');
 oppgave.style.display = 'none';
 
@@ -34,4 +36,17 @@ function oppgaver() {
     spillh1.innerHTML = 'Oppgave';
     spill1.style.display = 'none';
     oppgave.style.display = 'block';
+}
+
+
+function oppnedknappopp(tagElement) {
+    tagElement = tallid.innerHTML;
+    tagElement = tagElement -+ -1;
+    tallid.innerHTML = tagElement;
+}
+
+function oppnedknappned(tagElement) {
+    tagElement = tallid.innerHTML;
+    tagElement = tagElement -+ 1;
+    tallid.innerHTML = tagElement;
 }
