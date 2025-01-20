6a0dd7c60afd1e2eb4f28ea9b089f3ce8ad82007
MrKleivan <163395188+MrKleivan@users.noreply.github.com>
2025-01-20 15:05:24 +0100

    sdvsdv

---
+++ Emne3/ObligEmne3/ObligEmne3/Program.cs
@@ -0,0 +1,18 @@
+﻿using ObligEmne3;
+
+var app = new CountriesWithNeighborsByLand();
+
+var countryCodeConsole = new CountryCodeConsole("Velg et land: ");
+var countryName = countryCodeConsole.AskForCountry();
+Console.Clear();
+Console.WriteLine(countryName);
+Thread.Sleep(1500);
+
+while (true)
+{
+    Console.Clear();
+    List<Country> CountryList = app.GetListOfCountries();
+    var countryQuiz = new GuessNeighborsOfCountryQuiz(CountryList);
+    countryQuiz.Run();
+    countryQuiz.ShowResult();
+}
