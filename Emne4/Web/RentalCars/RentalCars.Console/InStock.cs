2073dfb8b4183bad3ecbe6568af60a6a1b7c4e08
MrKleivan <163395188+MrKleivan@users.noreply.github.com>
2025-01-23 11:00:08 +0100

    mmhn

---
+++ Emne4/Web/RentalCars/RentalCars.Console/InStock.cs
@@ -0,0 +1,15 @@
+namespace RentalCars.Console;
+
+public class InStock
+{
+    public List<Car> cars { get; set; }
+
+    public InStock()
+    {
+        cars = new List<Car>();
+
+        
+    }
+    
+    
+}
