0c99ac912a11e7553037c52c38ebf17dad27081a
MrKleivan <163395188+MrKleivan@users.noreply.github.com>
2025-02-25 13:38:05 +0100

    bfdbfdb

--- FuelRecorder/Backend/Fuelrecorder/Fuelrecorder/Models/FuelRecorderModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Fuelrecorder.Models;
-
-public class FuelRecorderModel
-{
-    public int Id { get; set; }
-    public double Kilometer { get; set; }
-    public double FuelFilled { get; set; }
-    public double Price { get; set; }
-}
